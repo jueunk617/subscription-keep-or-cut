3a0d7f8319d15077972366f09b5d6a1b8a04f69f
jueunk617 <juuuune62@naver.com>
2026-02-13 19:46:02 +0900

    feat(front): 프론트엔드 Tailwind v4 설정 및 루트 디렉토리 정리

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  turbopack: {
+    // 현재 폴더를 기준으로 모든 패키지를 찾도록 강제 설정
+    root: ".",
+  },
 };
 
 export default nextConfig;
